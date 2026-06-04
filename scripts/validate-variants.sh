4fb70e12b760f0c7ad596e84883bb67c279e4e8c
rinekri <rinekri@gmail.com>
2026-06-04 13:59:16 +0100

    Add api36-37 and api37 variants

--- scripts/validate-variants.sh
+++ scripts/validate-variants.sh
@@ -27,7 +27,9 @@ expected_ids_json='[
   "api35-36-j17-21",
   "api35-36-j17-21-23-ndk26-emu-api35",
   "api35-36-j17-23",
-  "api35-36-j17-23-ndk29-u24-gcc14"
+  "api35-36-j17-23-ndk29-u24-gcc14",
+  "api36-37-j17-21-23",
+  "api37-j17-21-23"
 ]'
 
 jq -e '.image_repo | type == "string" and length > 0' "${variants_file}" >/dev/null
